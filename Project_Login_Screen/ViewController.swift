3af78a6e7c988aae8a4b28bf91b84d13b8e39b63
Hitish <hitish1.sharma1@gmail.com>
2020-10-23 10:51:29 +0530

    Initial Commit

---
+++ Project_Login_Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_Login_Screen
+//
+//  Created by kunal sharma on 10/23/20.
+//  Copyright © 2020 Hitish sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project_Login_ScreenTests/Project_Login_ScreenTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_Login_ScreenTests.swift
+//  Project_Login_ScreenTests
+//
+//  Created by kunal sharma on 10/23/20.
+//  Copyright © 2020 Hitish sharma. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Login_Screen
+
+class Project_Login_ScreenTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
